8e2ca3463a986f17c01ba9d7c115611b57c5d320
Michael Mandujano <michaelmandujano4334@gmail.com>
2017-10-30 17:31:54 -0500

    Created Water class

---
+++ src/tiles/Water.java
@@ -0,0 +1,16 @@
+package tiles;
+
+import javafx.scene.image.Image;
+
+public class Water extends Obstacle {
+
+	private Image flameImage; // = *cactus file name here*;
+	
+	public Water(int x, int y) {
+		super(x, y);
+		//super.setImg(cactusImage);
+	}
+
+	
+
+}
