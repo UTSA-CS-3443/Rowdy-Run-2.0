c6ef6f90f1471de720929ca450a0feb9b63bac2a
Lynn Miguel <lynnsmiguel@yahoo.com>
2017-10-31 09:45:45 -0500

    Added Item Class

---
+++ src/tiles/Item.java
@@ -0,0 +1,41 @@
+package tiles;
+
+import java.awt.Rectangle;
+
+public class Item extends Tile {
+	
+	private Rectangle hitBox;	// Collision Detection
+	
+	/**
+	 * Current Position of the Object
+	 * 
+	 * Coordinates Given By Upper-Left Hand Corner of Item
+	 * Position Must Be In Bounds
+	 * 
+	 * 0 <= position_x <= maximum_x
+	 * 0 <= position_y <= maximum_y
+	 * 
+	 */
+	public int position_x;
+	public int position_y;
+	
+	/**
+	 * Size of Item, in Pixels
+	 */
+	public int item_width;
+	public int item_height;
+	
+	/**
+	 * Upper Bound area in which the Item can be placed
+	 * Maximum x and y positions for Upper-Left Hand Corner of Item
+	 * 
+	 */
+	public int maximum_x;
+	public int maximum_y;
+	
+	
+	public Item(int x, int y) {
+		super (x, y);
+					
+	}	
+}
