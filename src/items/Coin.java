63591e04614c71f6eff10a13431f5ce4efba42c4
Lynn Miguel <lynnsmiguel@yahoo.com>
2017-11-14 13:00:56 -0600

    Changes to items package

--- src/items/Coin.java
+++ src/items/Coin.java
@@ -1,21 +1,67 @@
 package items;
 
-import java.awt.Rectangle;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import com.sun.javafx.scene.traversal.Direction;
+
 
 public class Coin extends Item {
 	
-	private static int coins = 0;		// Counts Coins
-	private Rectangle hitBox;			// Collision Detection
+	public static String[] imgFile = {"Coin1.gif", "Coin2.gif", "Coin3.gif"};
 	
-	public static int getCoinCount() {
-		return coins;
+	public int start;
+	public static int start_vel_x = 0;
+	public static int start_vel_y = 0;
+	
+	public static int x_vel;
+	
+	public static final int WIDTH = 20;
+	public static final int HEIGHT = 20;
+	
+	public static final int start_height = 100;
+	
+	private BufferedImage[] image;
+	
+
+    public Coin(int start, int start_vel_x, int start_vel_y, int start_height, int WIDTH, int HEIGHT, Direction dir) {
+        super(start, start_vel_x, start_vel_y, start_height, WIDTH, HEIGHT, dir);
+        
+		try {
+			image = new BufferedImage[imgFile.length];
+			for (int i = 0; i < imgFile.length; i++) {
+				image[i] = ImageIO.read(new File(imgFile[i]));
+			}
+			if (img == null) img = image[0];
+		}	catch(IOException e) {
+				System.out.println("Error");
+		}
 	}
 	
-	public Coin(int x, int y) {
-		super (x, y);
+	public void move() {
+		if (x_vel > 0) dir = Direction.RIGHT;
+		if (x_vel < 0) dir = Direction.LEFT;
+		x_pos += x_vel;
+			
+		offScreen();
 	}
 	
+	public void spinCoin() {
+		for (int i = 0; i < image.length; i++) {
+			if (i == image.length - 1 && img.equals(image[i])) {
+				img = image[0];
+				break;
+			} else if (img.equals(image[i])) {
+				break;
+			}
+		}
+		
+	}
 	
+	public void offScreen() {
+		if (y_pos < 0) y_pos = 0;
+		else if (y_pos > y_max) y_pos= y_max;
+	}
 }
-	
-	
--- src/items/Item.java
+++ src/items/Item.java
@@ -1,43 +1,95 @@
 package items;
 
-import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import com.sun.javafx.scene.traversal.Direction;
 
-import tiles.Tile;
-
-public class Item extends Tile {
+public abstract class Item {
 	
-	private Rectangle hitBox;	// Collision Detection
+	protected BufferedImage img;
+	public Direction dir;
 	
 	/**
 	 * Current Position of the Object
 	 * 
-	 * Coordinates Given By Upper-Left Hand Corner of Item
-	 * Position Must Be In Bounds
-	 * 
-	 * 0 <= position_x <= maximum_x
-	 * 0 <= position_y <= maximum_y
-	 * 
+	 * 0 <= x_pos <= x_max
+	 * 0 <= y_pos <= y_max
 	 */
-	public int position_x;
-	public int position_y;
+	public int x_pos;
+	public int y_pos;
+	
+	/**
+	 * Pixels to Move Each Time move() is Called
+	 */
+	public int x_vel;
+	public int y_vel;
+	
+	/**
+	 * Bounds Area, Items Position
+	 * Maximum x and y Positions
+	 */
+	public int x_max;
+	public int y_max;
 	
 	/**
 	 * Size of Item, in Pixels
 	 */
-	public int item_width;
-	public int item_height;
+	public int width;
+	public int height;
 	
+	public Item(Direction dir, int x_pos, int y_pos, int x_vel, int y_vel, int x_max,
+			int y_max, int width, int height, int boxWidth, int boxHeight) {
+		this.dir = dir;
+		this.x_pos = x_pos;
+		this.y_pos = y_pos;
+		this.x_vel = x_vel;
+		this.y_vel = y_vel;
+		this.x_max = x_max;
+		this.y_max = y_max;
+		this.width = width;
+		this.height = height;
+	}
+
 	/**
-	 * Upper Bound area in which the Item can be placed
-	 * Maximum x and y positions for Upper-Left Hand Corner of Item
-	 * 
+	 * Move Item By Velocity
 	 */
-	public int maximum_x;
-	public int maximum_y;
+	public abstract void move();
+	public abstract void offScreen();
 	
+	public boolean intersect(Item coin) {
+		return (intersectRight(coin) || intersectTop(coin) || intersectBottom(coin));
+	}
 	
-	public Item(int x, int y) {
-		super (x, y);
-					
-	}	
+	public boolean intersectRight(Item coin) {
+		return (coin.x_pos <= x_pos + width && coin.x_pos >= x_pos + ((double) width / 2)
+				&& coin.y_pos + coin.height >= y_pos && coin.y_pos <= y_pos + height);
+	}
+	
+	public boolean intersectTop(Item coin) {
+		return (coin.y_pos + coin.height >= y_pos && coin.y_pos + coin.height <=y_pos + ((double) height / 2)
+				&& coin.x_pos + coin.width >= x_pos && coin.x_pos <= x_pos + width);
+	}
+	
+	public boolean intersectBottom(Item coin) {
+		return (coin.y_pos <= y_pos + height && coin.y_pos > y_pos + ((double) height / 2)
+				&& coin.x_pos + coin.width >= x_pos && coin.x_pos <= x_pos + width);
+	}
+	
+	public boolean collision(Item coin) {
+		return (collisionRight(coin) || collisionTop(coin) || collisionBottom(coin));
+	}
+	
+	public boolean collisionRight(Item coin) {
+		return (coin.x_pos == x_pos + width && coin.y_pos + coin.height >= y_pos
+				&& coin.y_pos <= y_pos + height);
+	}
+	
+	public boolean collisionTop(Item coin) {
+		return (coin.y_pos +coin.height == y_pos && coin.x_pos + coin.width >= x_pos
+				&& coin.x_pos <= x_pos + width);
+	}
+	
+	public boolean collisionBottom(Item coin) {
+		return (coin.y_pos == y_pos + height && coin.x_pos + coin.width >= x_pos
+				&& coin.x_pos <= x_pos + width);
+	}
 }
