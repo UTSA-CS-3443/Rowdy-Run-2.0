516500a31a4f7e81a77838e42469083a1ffc070c
jaredpolw <jaredpolwort@gmail.com>
2017-12-05 23:52:40 -0600

    javadocs

--- src/game/model/Rowdy.java
+++ src/game/model/Rowdy.java
@@ -24,9 +24,12 @@ public class Rowdy {
 	private int jumpTime, fallTime;
 	private boolean onGround, canMoveLeft, canMoveRight;
 	private int coinCount;
-	public static final int HEIGHT = 15;
-	public static final int WIDTH = 7;
+	protected static final int HEIGHT = 15;
+	protected static final int WIDTH = 7;
 
+	/**
+	 * Basic initialization method to make sure no field remains unassigned
+	 */
 	public Rowdy() {
 		this.position = new Point(20, 40);
 		this.xVelocity = 0;
@@ -43,6 +46,13 @@ public class Rowdy {
 		this.coinCount = 0;
 	}
 
+	/**
+	 * This method manually moves Rowdy and his hitboxes to a specified x,y
+	 * coordinate
+	 * 
+	 * @param x
+	 * @param y
+	 */
 	public void adjustRowdy(int x, int y) {
 		this.position.setLocation(x, y);
 		Point foot = new Point(this.position);
@@ -59,6 +69,10 @@ public class Rowdy {
 		this.rightBox.setLocation(right);
 	}
 
+	/**
+	 * This method is used directly by the model to use his derived velocities to
+	 * move him across the level
+	 */
 	public void moveRowdy() {
 		this.position.translate(this.xVelocity, yVelocity);
 		this.hitBox.translate(this.xVelocity, yVelocity);
@@ -68,20 +82,34 @@ public class Rowdy {
 		this.rightBox.translate(this.xVelocity, yVelocity);
 	}
 
+	/**
+	 * Called by the controller to give Rowdy velocity to the left
+	 */
 	public void moveLeft() {
 		if (this.canMoveLeft)
 			this.xVelocity = -1;
 	}
 
+	/**
+	 * Called by the controller to give Rowdy velocity to the right
+	 */
 	public void moveRight() {
 		if (this.canMoveRight)
 			this.xVelocity = 1;
 	}
 
+	/**
+	 * Used by the controller on key release to stop Rowdy's horizontal momentum
+	 */
 	public void stopHorizontalMotion() {
 		this.xVelocity = 0;
 	}
 
+	/**
+	 * This method used by the controller makes Rowdy simulate a jump by giving him
+	 * an initial velocity and a buffer for "gravity" to wear down before it
+	 * decelerates his jump
+	 */
 	public void jump() {
 		if (this.onGround) {
 			this.jumpTime = 15;
@@ -90,10 +118,17 @@ public class Rowdy {
 		}
 	}
 
+	/**
+	 * Used within the model to stop Rowdy's vertical momentum as a helper method
+	 */
 	protected void land() {
 		this.yVelocity = 0;
 	}
 
+	/**
+	 * Simulates gravity within the model by decelerating Rowdy whenever he isn't in
+	 * contact with ground through a buffer to limit the rate of deceleration
+	 */
 	public void fall() {
 		if (this.onGround && this.jumpTime == 0) {
 			this.yVelocity = 0;
@@ -112,26 +147,61 @@ public class Rowdy {
 			this.jumpTime -= 1;
 	}
 
+	/**
+	 * returns the amount of coins left to be drawn to the view
+	 * 
+	 * @return
+	 */
 	public int getCoinCount() {
 		return this.coinCount;
 	}
 
+	/**
+	 * returns Rowdy's position
+	 * 
+	 * @return Point where Rowdy currently is
+	 */
 	public Point getPosition() {
 		return position;
 	}
 
+	/**
+	 * Sets Rowdy's current position without care for his hitboxes
+	 * 
+	 * @param position
+	 */
 	public void setPosition(Point position) {
 		this.position = position;
 	}
 
+	/**
+	 * returns Rowdy's stored sprite for drawing
+	 * 
+	 * @return
+	 */
 	public Image getImg() {
 		return rowdySprite;
 	}
 
+	/**
+	 * Sets Rowdy's current sprite
+	 * 
+	 * @param rowdySprite
+	 */
 	public void setImg(Image rowdySprite) {
 		this.rowdySprite = rowdySprite;
 	}
 
+	/**
+	 * This method handles all of the collision of Rowdy by checking which blocks he
+	 * is interacting with and determining how to react to them depending on what
+	 * block type they are
+	 * 
+	 * @param level
+	 *            the currentlevel that Rowdy is navigating
+	 * @return 0 if everything is going fine 1 if rowdy died 2 if rowdy has won the
+	 *         game
+	 */
 	public int hitBoxChecker(Level level) {
 		this.onGround = false;
 		this.canMoveLeft = true;
@@ -146,11 +216,11 @@ public class Rowdy {
 		for (int i = 0; i < footlist.length; i++) {
 			if (footlist[i].y < 0 || footlist[i].y > level.HEIGHT)
 				return 1;
-			curr = level.access(footlist[i].x,footlist[i].y);
+			curr = level.access(footlist[i].x, footlist[i].y);
 			switch (curr.getTileType()) {
 			case 'C':
 				if (this.hitBox.intersects(curr.getHitBox()))
-					level.collectCoin(footlist[i].x,footlist[i].y);
+					level.collectCoin(footlist[i].x, footlist[i].y);
 				break;
 			case 'A':
 				break;
@@ -158,7 +228,7 @@ public class Rowdy {
 				if (this.footBox.intersects(curr.getHitBox())) {
 					this.onGround = true;
 					if (this.jumpTime == 0)
-						this.adjustRowdy(this.position.x, (int) curr.getPosition().getY() + (Rowdy.HEIGHT-4));
+						this.adjustRowdy(this.position.x, (int) curr.getPosition().getY() + (Rowdy.HEIGHT - 4));
 				}
 				break;
 			case 'G':
@@ -168,6 +238,7 @@ public class Rowdy {
 						this.adjustRowdy(this.position.x, (int) curr.getPosition().getY() + Rowdy.HEIGHT);
 				}
 				break;
+			case 'F':
 			case 'X':
 				if (this.hitBox.intersects(curr.getHitBox()))
 					return 1;
@@ -184,11 +255,11 @@ public class Rowdy {
 		for (int i = 0; i < headlist.length; i++) {
 			if (headlist[i].y < 0 || headlist[i].y > level.HEIGHT)
 				return 1;
-			curr = level.access(headlist[i].x,headlist[i].y);
+			curr = level.access(headlist[i].x, headlist[i].y);
 			switch (curr.getTileType()) {
 			case 'C':
 				if (this.hitBox.intersects(curr.getHitBox()))
-					level.access(headlist[i].x,headlist[i].y);
+					level.access(headlist[i].x, headlist[i].y);
 				break;
 			case 'P':
 			case 'A':
@@ -200,6 +271,7 @@ public class Rowdy {
 					this.adjustRowdy(this.position.x, (int) curr.getPosition().getY() - (Tile.HEIGHT + 2));
 				}
 				break;
+			case 'F':
 			case 'X':
 				if (this.hitBox.intersects(curr.getHitBox()))
 					return 1;
@@ -217,11 +289,11 @@ public class Rowdy {
 			this.adjustRowdy(10, this.position.y);
 		} else {
 			for (int i = 0; i < leftlist.length; i++) {
-				curr = level.access(leftlist[i].x,leftlist[i].y);
+				curr = level.access(leftlist[i].x, leftlist[i].y);
 				switch (curr.getTileType()) {
 				case 'C':
 					if (this.hitBox.intersects(curr.getHitBox()))
-						level.access(leftlist[i].x,leftlist[i].y);
+						level.access(leftlist[i].x, leftlist[i].y);
 					break;
 				case 'P':
 				case 'A':
@@ -232,6 +304,7 @@ public class Rowdy {
 						this.adjustRowdy((int) curr.getPosition().getX() + Tile.WIDTH, this.position.y);
 					}
 					break;
+				case 'F':
 				case 'X':
 					if (this.hitBox.intersects(curr.getHitBox()))
 						return 1;
@@ -249,11 +322,11 @@ public class Rowdy {
 			this.adjustRowdy(((level.WIDTH - 3) * Tile.WIDTH) + 9, this.position.y);
 		} else {
 			for (int i = 0; i < rightlist.length; i++) {
-				curr = level.access(rightlist[i].x,rightlist[i].y);
+				curr = level.access(rightlist[i].x, rightlist[i].y);
 				switch (curr.getTileType()) {
 				case 'C':
 					if (this.hitBox.intersects(curr.getHitBox()))
-						level.collectCoin(rightlist[i].x,rightlist[i].y);
+						level.collectCoin(rightlist[i].x, rightlist[i].y);
 					break;
 				case 'P':
 				case 'A':
@@ -264,6 +337,7 @@ public class Rowdy {
 						this.adjustRowdy((int) curr.getPosition().getX() - Rowdy.WIDTH, this.position.y);
 					}
 					break;
+				case 'F':
 				case 'X':
 					if (this.hitBox.intersects(curr.getHitBox()))
 						return 1;
@@ -274,7 +348,7 @@ public class Rowdy {
 				}
 			}
 		}
-		if(level.isLevelBeat())
+		if (level.isLevelBeat())
 			return 2;
 		return 0;
 	}
