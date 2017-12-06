66d2253c9b56fc5dcbfa3a6ce8a2ba56d537bbfb
Lynn Miguel <lynnsmiguel@yahoo.com>
2017-12-06 00:18:22 -0600

    Edit to Model to test

--- src/game/model/Model.java
+++ src/game/model/Model.java
@@ -255,6 +255,10 @@ public class Model implements Runnable {
 					gc.fillRect(currentLevel.access(x, y).getPosition().getX(),
 							Main.HEIGHT - (currentLevel.access(x, y).getPosition().getY() - 4), Tile.WIDTH,
 							Tile.HEIGHT - 8);
+				} else if (currentLevel.accessType(x, y) == 'X') {
+					gc.setFill(Color.GREEN);
+					gc.fillRect(currentLevel.access(x, y).getPosition().getX(),
+							Main.HEIGHT - currentLevel.access(x, y).getPosition().getY(), Tile.WIDTH, Tile.HEIGHT);
 				}
 			}
 		}
