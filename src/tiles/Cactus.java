032105605b578115bfe8035d09025b96aa7fdc8a
Lynn Miguel <lynnsmiguel@yahoo.com>
2017-12-06 00:11:38 -0600

    Edit Tile package

--- src/tiles/Cactus.java
+++ src/tiles/Cactus.java
@@ -1,27 +1,33 @@
 package tiles;
 
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
-import javafx.scene.image.Image;
-/**
- * 
- * represents the cactus tile
- * 
- * @author Lynn
- *
- */
+import javax.imageio.ImageIO;
+
+
+
 public class Cactus extends Obstacle {
 
 	/**
 	 * Set Cactus Image
 	 */
-	Image cactusSprite = new Image("/src/images/cactus.png");
+	// Image cactusSprite = new Image("/src/images/cactus.png");
 
+	BufferedImage img;
+	
 	// Constructor
 	public Cactus(int x, int y) {
 		super(x, y);
 		this.tileType = 'X';
-	}
+
+	    try {                
+	        img = ImageIO.read(getClass().getResourceAsStream("/src/images/cactus.png"));
+	    }
+	    catch(Exception e) {
+	        e.printStackTrace();
+	        }
+	    }
 
 	public Rectangle getHitBox() {
 		return hitBox;
