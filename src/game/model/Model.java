cc0880db9c2a80b889d9b139503becd9afa650be
jaredpolw <jaredpolwort@gmail.com>
2017-12-06 23:43:31 -0600

    commiting collision fixes and graphical changes for realsies now?

---
+++ src/game/model/Model.java
@@ -0,0 +1,291 @@
+package game.model;
+
+import java.awt.Point;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+import javafx.event.ActionEvent;
+
+import tiles.*;
+import game.Main;
+import game.controller.GameController;
+import items.Coin;
+
+/**
+ * Reads in data from a level file (lvlFile) to generate a level consisting of
+ * Tile objects stored in an ArrayList.
+ * 
+ * @author Jared Polwort
+ * @author Michael Diep
+ *
+ */
+public class Model implements Runnable {
+
+	protected Level currentLevel;
+	protected Rowdy player = new Rowdy();
+	private int playerState;
+	// private Timer timer;
+	// private TimerListener timerListener;
+
+	private Timeline indefiniteTimeline;
+	private KeyFrame kf;
+	private EventHandler<ActionEvent> timelineHandler;
+
+	// Canvas is initialized by the GameView.FXML
+	private Canvas canvas = null;
+	private GraphicsContext gc = null;
+
+	public Model() {
+
+		indefiniteTimeline = new Timeline();
+		indefiniteTimeline.setCycleCount(Timeline.INDEFINITE);
+
+		timelineHandler = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				gameTick();
+			}
+		};
+
+		// kf = new KeyFrame(Duration.seconds(.0017), timelineHandler); //60 fps
+		kf = new KeyFrame(Duration.seconds(.0066), timelineHandler); // 15 fps
+		indefiniteTimeline.getKeyFrames().add(kf);
+
+	}
+
+	@Override
+	public void run() {
+		if (gc != null)
+			indefiniteTimeline.play();
+		else
+			System.out.println(" ");
+
+	}
+
+	public void gameTick() {
+		// TODO Fix this stuff, it ai'nt workin'
+		int playerState;
+		// player.moveRowdy();
+		playerState = player.hitBoxChecker(currentLevel);
+		player.fall();
+		player.moveRowdy();
+		// player.stop();
+		if (playerState == 1) {
+			// TODO handle rowdy dying
+			System.err.println("You died");
+			player.adjustRowdy(currentLevel.playerStart.x, currentLevel.playerStart.y);
+		} else if (playerState == 2) {
+			// TODO handle rowdy winning
+			System.err.println("YOU WIN!");
+			playerState = 0;
+			player.setVelocity(0, 0);
+			System.out.println("Ending Game");
+			this.indefiniteTimeline.pause();
+			//Main.setModel(new Model());
+			Main.changeScene(Main.getMainMenu());
+			//delete this model somehow
+		}
+		// player.fall();
+		try {
+			drawCanvas(gc);
+		} catch (NullPointerException e) {
+			System.out.println("No GraphicsContext present");
+		}
+	}
+
+	public int getPlayerState() {
+		return this.playerState;
+	}
+	
+	public Level getCurrentLevel() {
+		return currentLevel;
+	}
+
+	public void setCurrentLevel(Level currentLevel) {
+		this.currentLevel = currentLevel;
+		player.adjustRowdy(currentLevel.playerStart.x, currentLevel.playerStart.y);
+	}
+
+	public Rowdy getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Rowdy player) {
+		this.player = player;
+	}
+
+	public Timeline getIndefiniteTimeline() {
+		return indefiniteTimeline;
+	}
+
+	public void setIndefiniteTimeline(Timeline indefiniteTimeline) {
+		this.indefiniteTimeline = indefiniteTimeline;
+	}
+
+	public Canvas getCanvas() {
+		return canvas;
+	}
+
+	public void setCanvas(Canvas canvas) {
+		this.canvas = canvas;
+	}
+
+	public GraphicsContext getGraphicsContext() {
+		return gc;
+	}
+
+	public void setGraphicsContext(GraphicsContext gc) {
+		this.gc = gc;
+	}
+
+	/**
+	 * Reads in the txt file corresponding to the level and gives a nicely formatted
+	 * level object
+	 * 
+	 * @param lvlFile
+	 *            - text file that has the structure of the level
+	 * @return Level - an object that has the format of the level in a easily read
+	 *         and manipulatable format
+	 * @throws IOException
+	 */
+	public Level readLevel(File lvlFile) throws IOException {
+
+		ArrayList<Tile[]> Level = new ArrayList<Tile[]>();
+		Point rowdyPosition = new Point(20, 40);
+		int x = 0;
+		Scanner in = null;
+		try {
+			in = new Scanner(lvlFile);
+		} catch (FileNotFoundException exception) {
+			System.err.println("Did not find a level of that name");
+			System.exit(1);
+		}
+		while (in.hasNextLine()) {
+			Tile[] levelColumn = null;
+			String line = in.nextLine();
+			levelColumn = processLevelColumn(line.toCharArray(), x, rowdyPosition);
+			Level.add(levelColumn);
+			x++;
+		}
+		Level finalLevel = new Level(Level, rowdyPosition);
+		return finalLevel;
+	}
+
+	/**
+	 * Helper method to readLevel which parses a column to be added to the arrayList
+	 * of tile arrays
+	 * 
+	 * @param tileList
+	 * @param column
+	 * @return
+	 */
+	private Tile[] processLevelColumn(char[] tileList, int column, Point start) {
+		// TODO add tile objects as you create them
+		// System.out.println("generating air in column " + column);
+		Tile[] levelColumn = new Tile[200];// Once air tile is created will just be initialized as twenty-five of those
+		for (int i = 0; i < levelColumn.length; i++)
+			levelColumn[i] = new Air(column * Tile.WIDTH, Main.HEIGHT - i * Tile.HEIGHT);
+
+		// System.out.println("generating level from tileList[" + column + "]");
+		for (int i = 0; i < tileList.length; i++) {
+			Tile currTile = null;
+			switch (tileList[i]) {
+			case 'F':
+				currTile = new Flame(column * Tile.WIDTH, (i + 1) * Tile.HEIGHT);
+				break;
+			case 'W':
+				currTile = new Water(column * Tile.WIDTH, (i + 1) * Tile.HEIGHT);
+				break;
+			case 'X':
+				currTile = new Cactus(column * Tile.WIDTH, (i + 1) * Tile.HEIGHT);
+				break;
+			case 'G':
+				currTile = new Ground(column * Tile.WIDTH, (i + 1) * Tile.HEIGHT);
+				break;
+			case 'P':
+				currTile = new Platform(column * Tile.WIDTH, (i + 1) * Tile.HEIGHT);
+				break;
+			case 'C':
+				currTile = new Coin(column * Tile.WIDTH, (i + 1) * Tile.HEIGHT);
+				break;
+			case 'R':
+				start.setLocation(column * Tile.WIDTH, (i + 1) * Tile.HEIGHT);
+			case ' ':
+				currTile = new Air(column * Tile.WIDTH, (i + 1) * Tile.HEIGHT);
+				break;
+			default:
+				break;
+			}
+			levelColumn[i] = currTile;
+		}
+		return levelColumn;
+	}
+
+	
+	public static int xOffset=0;
+	public static int yOffset=0;
+	
+	public void drawCanvas(GraphicsContext gc) {
+		// gc.fillRect(100, 100, 100, 100);
+		gc.clearRect(0, 0, 500, 500);
+		// gc.translate(1, 0);
+		for (int x = 0; x < currentLevel.WIDTH; x++) {
+			for (int y = 0; y < currentLevel.HEIGHT; y++) {
+				// System.out.println("drawing on canvas at " + temp[y].getPosition().getX() +
+				// ", " + temp[y].getPosition().getY());
+				Tile temp[] = currentLevel.getLayout().get(x);
+				//gc.drawImage(temp[y].getImg(), temp[y].getPosition().getX(), Main.HEIGHT - temp[y].getPosition().getY());
+				temp[y].drawTile(gc);
+				
+				// temp[y].getPosition().getY());
+				if (currentLevel.accessType(x, y) == 'G') {
+
+					gc.setFill(Color.SADDLEBROWN);
+					gc.fillRect(currentLevel.access(x, y).getPosition().getX(),
+							Main.HEIGHT - currentLevel.access(x, y).getPosition().getY(), Tile.WIDTH, Tile.HEIGHT);
+					gc.setFill(Color.BLACK);
+					gc.fillRect(currentLevel.access(x, y).getPosition().getX(), currentLevel.access(x, y).getPosition().getY(), Tile.WIDTH, Tile.HEIGHT);
+					// run this if you want severe lag
+				} else if (currentLevel.accessType(x, y) == 'A') {
+					gc.setFill(Color.AQUAMARINE);
+					// gc.fillRect(currentLevel.access.getPosition().getX(),
+					// currentLevel.access.getPosition().getY(), 10,
+					// 10);
+
+				} else if (currentLevel.accessType(x, y) == 'C') {
+					gc.setFill(Color.ORANGE);
+					gc.fillRect(currentLevel.access(x, y).getPosition().getX(), currentLevel.access(x, y).getPosition().getY(), Tile.WIDTH, Tile.HEIGHT);
+					// run this if you want severe lag
+				} else if (currentLevel.accessType(x, y) == 'P') {
+					gc.setFill(Color.BLACK);
+					gc.fillRect(currentLevel.access(x, y).getPosition().getX(),
+							Main.HEIGHT - (currentLevel.access(x, y).getPosition().getY() - 4), Tile.WIDTH,
+							Tile.HEIGHT - 8);
+				}
+			}
+		}
+		gc.setFill(Color.BLUE);
+		
+		//added values from offset to coords
+		gc.drawImage(this.player.getImg(),this.player.getPosition().getX()+xOffset, this.player.getPosition().getY()+yOffset - 1 + Rowdy.HEIGHT, Rowdy.WIDTH, -Rowdy.HEIGHT);
+		//gc.fillRect(this.player.getPosition().getX(), (this.player.getPosition().getY()), this.player.WIDTH, this.player.HEIGHT);
+		// gc.drawImage(this.player.getImg(), this.player.getPosition().getX(),
+		// this.player.getPosition().getY());
+	}
+
+}
---
+++ src/game/model/Rowdy.java
@@ -0,0 +1,384 @@
+package game.model;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+import javafx.scene.image.Image;
+import tiles.Air;
+import tiles.Tile;
+
+/**
+ * Handles all of the main character's movement and hitbox detection as well as
+ * storing his sprite
+ * 
+ * @author Jared Polwort
+ *
+ */
+public class Rowdy {
+
+	protected Point position;
+	private int xVelocity, yVelocity;
+	private Rectangle hitBox, footBox, headBox, rightBox, leftBox;
+	protected Image rowdySprite;
+	private int jumpTime, fallTime;
+	private boolean onGround, canMoveLeft, canMoveRight;
+	private int coinCount;
+	private boolean canJump;
+	protected static final int HEIGHT = 15;
+	protected static final int WIDTH = 7;
+
+	/**
+	 * Basic initialization method to make sure no field remains unassigned
+	 */
+	public Rowdy() {
+		Image img;
+		this.position = new Point(20, 40);
+		this.xVelocity = 0;
+		this.yVelocity = 0;
+		this.jumpTime = 0;
+		this.onGround = false;
+		this.canMoveLeft = true;
+		this.canMoveRight = true;
+		this.hitBox = new Rectangle(this.position.x, this.position.y, Rowdy.WIDTH, Rowdy.HEIGHT);
+		this.headBox = new Rectangle(this.position.x + 2, this.position.y+1, Rowdy.WIDTH-4, 2);
+		this.footBox = new Rectangle(this.position.x + 1, this.position.y - (Rowdy.HEIGHT - 3), Rowdy.WIDTH - 2, 3);
+		this.rightBox = new Rectangle(this.position.x + (Rowdy.WIDTH - 2), this.position.y + 2, 2, Rowdy.HEIGHT - 2);
+		this.leftBox = new Rectangle(this.position.x, this.position.y + 2, 2, Rowdy.HEIGHT - 2);
+		
+		try {                
+	        img = new Image("images/Rowdy16.gif", this.WIDTH, this.HEIGHT, false, false);
+	        this.setImg(img);
+	    }
+	    catch(Exception e) {
+	        e.printStackTrace();
+	        }
+		
+	}
+
+	/**
+	 * This method manually moves Rowdy and his hitboxes to a specified x,y
+	 * coordinate
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void adjustRowdy(int x, int y) {
+		this.position.setLocation(x, y);
+		Point foot = new Point(this.position);
+		Point head = new Point(this.position);
+		Point right = new Point(this.position);
+		foot.translate(0, -(Rowdy.HEIGHT - 1));
+		head.translate(0, 1);
+		right.translate((Rowdy.WIDTH - 2), 0);
+
+		this.hitBox.setLocation(this.position);
+		this.leftBox.setLocation(this.position);
+		this.headBox.setLocation(head);
+		this.footBox.setLocation(foot);
+		this.rightBox.setLocation(right);
+	}
+
+	/**
+	 * This method is used directly by the model to use his derived velocities to
+	 * move him across the level
+	 */
+	public void moveRowdy() {
+		this.position.translate(this.xVelocity, yVelocity);
+		this.hitBox.translate(this.xVelocity, yVelocity);
+		this.headBox.translate(this.xVelocity, yVelocity);
+		this.footBox.translate(this.xVelocity, yVelocity);
+		this.leftBox.translate(this.xVelocity, yVelocity);
+		this.rightBox.translate(this.xVelocity, yVelocity);
+	}
+
+	/**
+	 * Called by the controller to give Rowdy velocity to the left
+	 */
+	public void moveLeft() {
+		if (this.canMoveLeft)
+			this.xVelocity = -1;
+	}
+
+	/**
+	 * Called by the controller to give Rowdy velocity to the right
+	 */
+	public void moveRight() {
+		if (this.canMoveRight)
+			this.xVelocity = 1;
+	}
+
+	/**
+	 * Used by the controller on key release to stop Rowdy's horizontal momentum
+	 * 
+	 * @param xV - x velocity
+	 * @param yV - y Velocity
+	 */
+	public void setVelocity(int xV, int yV) {
+		this.xVelocity = xV;
+		this.yVelocity = yV;
+	}
+	
+	
+
+	public int getXVelocity() {
+		return xVelocity;
+	}
+
+	public int getYVelocity() {
+		return yVelocity;
+	}
+
+	/**
+	 * This method used by the controller makes Rowdy simulate a jump by giving him
+	 * an initial velocity and a buffer for "gravity" to wear down before it
+	 * decelerates his jump
+	 */
+	public void jump() {
+		if (this.onGround) {
+			this.jumpTime = 15;
+			this.fallTime = 10;
+			this.yVelocity = 3;
+			this.onGround = false;
+			this.canJump = false;
+		}
+	}
+
+	/**
+	 * Used within the model to stop Rowdy's vertical momentum as a helper method
+	 */
+	protected void land() {
+		this.yVelocity = 0;
+	}
+
+	/**
+	 * Simulates gravity within the model by decelerating Rowdy whenever he isn't in
+	 * contact with ground through a buffer to limit the rate of deceleration
+	 */
+	public void fall() {
+		if (this.onGround && this.jumpTime == 0) {
+			//this.yVelocity = 0;
+			return;
+		}
+		if (this.jumpTime == 0) {
+			if (this.fallTime > 0) {
+				this.fallTime -= 1;
+				return;
+			}
+			this.yVelocity -= 1;
+			if (this.yVelocity < -3)
+				this.yVelocity = -3;
+			this.fallTime = 10;
+		} else
+			this.jumpTime -= 1;
+	}
+
+	/**
+	 * returns Rowdy's position
+	 * 
+	 * @return Point where Rowdy currently is
+	 */
+	public Point getPosition() {
+		return position;
+	}
+
+	/**
+	 * Sets Rowdy's current position without care for his hitboxes
+	 * 
+	 * @param position
+	 */
+	public void setPosition(Point position) {
+		this.position = position;
+	}
+
+	/**
+	 * returns Rowdy's stored sprite for drawing
+	 * 
+	 * @return
+	 */
+	public Image getImg() {
+		return rowdySprite;
+	}
+
+	/**
+	 * Sets Rowdy's current sprite
+	 * 
+	 * @param rowdySprite
+	 */
+	public void setImg(Image rowdySprite) {
+		this.rowdySprite = rowdySprite;
+	}
+
+	/**
+	 * This method handles all of the collision of Rowdy by checking which blocks he
+	 * is interacting with and determining how to react to them depending on what
+	 * block type they are
+	 * 
+	 * @param level
+	 *            the currentlevel that Rowdy is navigating
+	 * @return 0 if everything is going fine 1 if rowdy died 2 if rowdy has won the
+	 *         game
+	 */
+	public int hitBoxChecker(Level level) {
+		this.onGround = false;
+		this.canMoveLeft = true;
+		this.canMoveRight = true;
+		Tile curr = null;
+		int rowdyX = (int) Math.floor((double) this.position.x / Tile.WIDTH);
+		int rowdyY = (int) Math.floor((double) this.position.y / Tile.HEIGHT);
+
+		Point[] leftlist = { new Point(rowdyX, rowdyY), new Point(rowdyX, rowdyY - 1) };
+
+		if (rowdyX <= 0) {
+			canMoveLeft = false;
+			this.adjustRowdy(10, this.position.y);
+		} else {
+			for (int i = 0; i < leftlist.length; i++) {
+				curr = level.access(leftlist[i].x, leftlist[i].y);
+				switch (curr.getTileType()) {
+				case 'C':
+					if (this.hitBox.intersects(curr.getHitBox()))
+						level.collectCoin(leftlist[i].x, leftlist[i].y);
+					break;
+				case 'P':
+				case 'A':
+					break;
+				case 'G':
+					if (this.leftBox.intersects(curr.getHitBox())) {
+						this.canMoveLeft = false;
+						//this.xVelocity = 0;
+						this.adjustRowdy(this.position.x + 1, this.position.y);
+					}
+					break;
+				case 'W':
+				case 'F':
+				case 'X':
+					if (this.hitBox.intersects(curr.getHitBox()))
+						return 1;
+					break;
+				case '?':
+				default:
+					return 1;
+				}
+			}
+		}
+		Point[] rightlist = { new Point(rowdyX + 1, rowdyY), new Point(rowdyX + 1, rowdyY - 1),
+				new Point(rowdyX + 1, rowdyY - 2), new Point(rowdyX, rowdyY), new Point(rowdyX, rowdyY - 1) };
+		if (rowdyX >= level.WIDTH - 2) {
+			canMoveRight = false;
+			this.adjustRowdy(((level.WIDTH - 3) * Tile.WIDTH) + 9, this.position.y);
+		} else {
+			for (int i = 0; i < rightlist.length; i++) {
+				curr = level.access(rightlist[i].x, rightlist[i].y);
+				switch (curr.getTileType()) {
+				case 'C':
+					if (this.hitBox.intersects(curr.getHitBox()))
+						level.collectCoin(rightlist[i].x, rightlist[i].y);
+					break;
+				case 'P':
+				case 'A':
+					break;
+				case 'G':
+					if (this.rightBox.intersects(curr.getHitBox())) {
+						this.canMoveRight = false;
+						//this.xVelocity = 0;
+						this.adjustRowdy(this.position.x - 1, this.position.y);
+					}
+					break;
+				case 'W':
+				case 'F':
+				case 'X':
+					if (this.hitBox.intersects(curr.getHitBox()))
+						return 1;
+					break;
+				case '?':
+				default:
+					return 1;
+				}
+			}
+		}
+		
+		Point[] footlist = { new Point(rowdyX + 1, rowdyY - 1), new Point(rowdyX, rowdyY - 1),
+				new Point(rowdyX + 1, rowdyY - 2), new Point(rowdyX, rowdyY - 2) };
+
+		for (int i = 0; i < footlist.length; i++) {
+			if (footlist[i].y < 0 || footlist[i].y > level.HEIGHT)
+				return 1;
+			curr = level.access(footlist[i].x, footlist[i].y);
+			switch (curr.getTileType()) {
+			case 'C':
+				if (this.hitBox.intersects(curr.getHitBox()))
+					level.collectCoin(footlist[i].x, footlist[i].y);
+				break;
+			case 'A':
+				break;                    
+			case 'P':
+				if (this.footBox.intersects(curr.getHitBox())) {
+					this.onGround = true;
+					this.canJump = true;
+					if (this.jumpTime == 0)
+						this.adjustRowdy(this.position.x, (int) curr.getPosition().getY() + (Rowdy.HEIGHT - 4));
+				}
+				break;
+			case 'G':
+				if (this.footBox.intersectsLine(curr.getPosition().getX() + 1, curr.getPosition().getY() + 1, curr.getPosition().getX() - 1 + curr.WIDTH, curr.getPosition().getY() + 1)) {
+					this.onGround = true;
+					this.canJump = true;   //might use this makes the jump work more like a double jump
+					if (this.jumpTime == 0)
+						this.adjustRowdy(this.position.x, (int) curr.getPosition().getY() + Rowdy.HEIGHT);
+				}
+				break;
+			case 'W':
+			case 'F':
+			case 'X':
+				if (this.hitBox.intersects(curr.getHitBox()))
+					return 1;
+				break;
+			case '?':
+			default:
+				return 1;
+			}
+		}
+
+		Point[] headlist = { new Point(rowdyX + 1, rowdyY), new Point(rowdyX, rowdyY),
+				new Point(rowdyX + 1, rowdyY + 1), new Point(rowdyX, rowdyY + 1) };
+
+		for (int i = 0; i < headlist.length; i++) {
+			if (headlist[i].y < 0 || headlist[i].y > level.HEIGHT)
+				return 1;
+			curr = level.access(headlist[i].x, headlist[i].y);
+			switch (curr.getTileType()) {
+			case 'C':
+				if (this.hitBox.intersects(curr.getHitBox()))
+					level.collectCoin(headlist[i].x, headlist[i].y);
+				break;
+			case 'P':
+			case 'A':
+				break;
+			case 'G':
+				if (this.headBox.intersects(curr.getHitBox())) {
+					this.jumpTime = 0;
+					this.yVelocity = 0;
+					//this.land();
+					//this.adjustRowdy(this.position.x, (int) curr.getPosition().getY() - (Tile.HEIGHT+1));
+				}
+				break;
+			case 'W':
+			case 'F':
+			case 'X':
+				if (this.hitBox.intersects(curr.getHitBox()))
+					return 1;
+				break;
+			case '?':
+			default:
+				return 1;
+			}
+		}
+
+		
+		if (level.isLevelBeat())
+			return 2;
+		return 0;
+	}
+
+}
