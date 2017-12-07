93389166a9406b1bd752d017f10ed91b07711104
Lynn Miguel <lynnsmiguel@yahoo.com>
2017-12-06 20:27:42 -0600

    Removed some data

--- src/items/Coin.java
+++ src/items/Coin.java
@@ -1,11 +1,6 @@
 package items;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-
 import javafx.scene.image.Image;
 import tiles.Tile;
 
