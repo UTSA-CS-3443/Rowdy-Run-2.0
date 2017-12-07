df8c400677481b40d37ad565ac6995a1e5d4e17b
Lynn Miguel <lynnsmiguel@yahoo.com>
2017-12-06 20:22:09 -0600

    Fixed Coin Class

--- src/items/Coin.java
+++ src/items/Coin.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+import javafx.scene.image.Image;
 import tiles.Tile;
 
 /**
@@ -14,50 +15,26 @@ import tiles.Tile;
  *
  */
 public class Coin extends Tile {
-	
+
 	/**
-	 * Hold Image Files
+	 * Set Location of Water Sprite
+	 * Draw Image to Canvas
+	 * @param x
+	 * @param y
 	 */
-	public static String[] imgFile = {"Coin1.gif", "Coin2.gif", "Coin3.gif", 
-			"Coin1.gif", "Coin2.gif", "Coin3.gif", "Coin1.gif", "Coin2.gif", "Coin3.gif"};
-	
-	public static final int WIDTH = 20;
-	public static final int HEIGHT = 20;
-	private BufferedImage[] image;
-	
     public Coin(int x,int y) {
         super(x,y);
-        //hitBox = new Rectangle(x + 30, y + 30, WIDTH, HEIGHT);
         this.tileType = 'C';
-		/*try {
-			image = new BufferedImage[imgFile.length];
-			for (int i = 0; i < imgFile.length; i++) {
-				image[i] = ImageIO.read(new File(imgFile[i]));
-			}
-			if (img == null) img = image[0];
-		}	catch(IOException e) {
-			System.out.println("Error");
-		}*/
+        
+	    try {                
+	        this.img = new Image("images/Coin1.gif", Tile.WIDTH, Tile.HEIGHT, false, false);
+	    }
+	    catch(Exception e) {
+	        e.printStackTrace();
+	        }
 	}
     
     public Rectangle getHitBox() {
     	return hitBox;
     }
-    
-	/**
-	 * Loops Images Files and will Spin Coin
-	 */
-    /*
-	public void spinCoin() {
-		for (int i = 0; i < image.length; i++) {
-			if (i == image.length - 1 && img.equals(image[i])) {
-				img = image[0];
-				break;
-			} else if (img.equals(image[i])) {
-				break;
-			}
-		}
-		
-	}
-*/
 }
